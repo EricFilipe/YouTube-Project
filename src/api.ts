b8834849659111680056b9459877f9433b92732b
EricFilipe <ericfilipe125@gmail.com>
2024-04-23 10:45:26 -0300

    Trying Request

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: 'https://api-youtube-project.onrender.com',
     headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://api-youtube-project.onrender.com',
         'Content-Type': 'application/json',
     }
 })
