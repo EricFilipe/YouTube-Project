cf914176a66350adacacf952886447b5a5baff70
EricFilipe <ericfilipe125@gmail.com>
2024-04-24 12:29:46 -0300

    trying req

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://you-tube-project-phi.vercel.app',
+    baseURL: 'https://api-youtube-project.onrender.com',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
