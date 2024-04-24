7fc7fed1822d96bd778a71043eb2b3b432fd2bd6
EricFilipe <ericfilipe125@gmail.com>
2024-04-24 12:26:45 -0300

    trying req

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: '*',
+    baseURL: 'https://you-tube-project-phi.vercel.app',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
