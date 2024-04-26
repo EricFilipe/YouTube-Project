4027e1fa5af2b221ccb7eb08b0f5cabf24d998cb
EricFilipe <ericfilipe125@gmail.com>
2024-04-26 10:15:29 -0300

    trying to fix cors bug

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: 'https://api-youtube-project.onrender.com',
     headers: {
-        'Access-Control-Allow-Origin': 'https://you-tube-project-phi.vercel.app',
+        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
     }
 })
